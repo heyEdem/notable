f8eabf679da0ce342b9abff7a48fc58c760fc6ae
heyEdem <akatoaugustine@gmail.com>
2025-04-01 16:27:08 +0000

    first commit

---
+++ src/test/java/com/edem/notable/NotableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edem.notable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
